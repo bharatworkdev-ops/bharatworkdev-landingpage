75d2468670169c069700f4ec2a81707d92783a3a
bharatworkdev-ops <bharatworkdev@gmail.com>
2026-06-11 14:29:23 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -14,13 +14,6 @@ const Team = () => {
       role: "Co-Founder & CTO",
       description: "Operational leader ensuring seamless connections between daily wage workers, agents, and enterprise employers nationwide",
       image: "https://res.cloudinary.com/dgkmzddjv/image/upload/v1774187538/WhatsApp_Image_2026-03-22_at_6.16.21_PM_jk3nex.jpg",
-    },
-    ,
-    {
-      name: "Ayush Pandey",
-      role: "Head of Mobile App",
-      description: "Expert in mobile technologies ensuring the Labour and Employer apps run flawlessly on all devices.",
-      image: "https://ui-avatars.com/api/?name=Developer&background=27AE60&color=fff&size=150",
     }
   ];
 
